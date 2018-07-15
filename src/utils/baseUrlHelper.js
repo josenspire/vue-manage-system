2b8f52ff42f571ce4c1f4f5ef3c7993717c574e3
James <josenspire@gmail.com>
2018-07-15 20:30:12 +0800

    update base url

--- src/utils/baseUrlHelper.js
+++ src/utils/baseUrlHelper.js
@@ -1,1 +1,2 @@
-export default () => `${window.location.protocol}//${window.location.host}/wxmall/v1/api`;
+// export default () => `${window.location.protocol}//${window.location.host}/wxmall/v1/api/admin`;
+export default () => `${window.location.protocol}//${window.location.host}/v1/api/admin`;
